8b88804cc2c0b327f6a4e1edc9248d15fc5162c9
huggywuggybest03 <135007348+huggywuggybest03@users.noreply.github.com>
2023-06-15 16:47:30 +0100

    This is 6 different UI libraries
    These are UI libraries for coding. Do CTRL + Left Click to lead to that link.

---
+++ libraries.lua
@@ -0,0 +1,17 @@
+--1. LinoriaLib
+--https://github.com/violin-suzutsuki/LinoriaLib/blob/main/Example.lua
+
+--2. Ocerium-Project
+--https://github.com/slf0Dev/Ocerium_Project/blob/main/README.md
+
+--3. Rayfield
+--https://docs.sirius.menu/rayfield
+
+--4. Kavo UI
+--https://xheptcofficial.gitbook.io/kavo-library/
+
+--5. Orion
+--https://github.com/shlexware/Orion/blob/main/Documentation.md
+
+--Wally
+--https://github.com/bloodball/UI-Librarys/blob/main/Wally%20UI%20v3
