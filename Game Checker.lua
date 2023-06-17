df272bfb0f3ad677d35e4c25e20b0978e85f55bb
monkey_kid03 <135007348+huggywuggybest03@users.noreply.github.com>
2023-06-17 12:51:55 +0100

    Check Game
    Is it supported? Let's see!

---
+++ Game Checker.lua
@@ -0,0 +1,12 @@
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.2.8) ~  Much Love, Ferib 
+
+]]--
+
+if (game.PlaceId==155615604) then loadstring(game:HttpGet("https://raw.githubusercontent.com/huggywuggybest03/All-Our-Hubs/main/prison%20life%20op%20hub"))();elseif (game.PlaceId==1997193809) then loadstring(game:HttpGet("https://raw.githubusercontent.com/huggywuggybest03/All-Our-Hubs/main/Kavo%20UI/backpacking%20script"))();elseif (game.PlaceId==(402124064 -(986 + 87))) then loadstring(game:HttpGet("https://raw.githubusercontent.com/huggywuggybest03/All-Our-Hubs/main/redwood%20prison%20hub"))();elseif ((game.PlaceId==(12324140586 -690627695)) or (12387285154 -(993 + 357))) then loadstring(game:HttpGet("https://raw.githubusercontent.com/huggywuggybest03/All-Our-Hubs/main/Cart%20Ride%20But%20Youre%20A%20Ball"))();else local v0=619 -(555 + 64) ;local v1;while true do if (v0==(931 -(857 + 74))) then v1=game.Players.LocalPlayer;v1:Kick("Sorry, you need to be in the games: Prison Life, Backpacking, Redwood Prison, Cart Ride But You're A Ball or golf obby.");break;end end end
