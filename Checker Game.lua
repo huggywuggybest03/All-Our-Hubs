f12353ec94871ddc692b87b632a808a7be89aef4
monkey_kid03 <135007348+huggywuggybest03@users.noreply.github.com>
2023-06-17 13:04:38 +0100

    Update Checker Game.lua

--- Checker Game.lua
+++ Checker Game.lua
@@ -1,12 +1,1 @@
---[[
- .____                  ________ ___.    _____                           __                
- |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
- |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
- |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
- |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
-         \/          \/         \/    \/                \/     \/     \/                   
-          \_Welcome to LuaObfuscator.com   (Alpha 0.2.8) ~  Much Love, Ferib 
-
-]]--
-
 if (game.PlaceId==155615604) then loadstring(game:HttpGet("https://raw.githubusercontent.com/huggywuggybest03/All-Our-Hubs/main/prison%20life%20op%20hub"))();elseif (game.PlaceId==1997193809) then loadstring(game:HttpGet("https://raw.githubusercontent.com/huggywuggybest03/All-Our-Hubs/main/Kavo%20UI/backpacking%20script"))();elseif (game.PlaceId==402122991) then loadstring(game:HttpGet("https://raw.githubusercontent.com/huggywuggybest03/All-Our-Hubs/main/redwood%20prison%20hub"))();elseif (game.PlaceId==11633512891) then loadstring(game:HttpGet("https://raw.githubusercontent.com/huggywuggybest03/All-Our-Hubs/main/Cart%20Ride%20But%20Youre%20A%20Ball"))();elseif (game.PlaceId==12387283804) then loadstring(game:HttpGet("https://raw.githubusercontent.com/huggywuggybest03/All-Our-Hubs/main/Cart%20Ride%20But%20Youre%20A%20Ball"))();else local v0=game.Players.LocalPlayer;v0:Kick("Sorry, you need to be in the games: Prison Life, Backpacking, Redwood Prison, Cart Ride But You're A Ball or golf obby.");end
