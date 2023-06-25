20ce3b29a45bee68f7e44580931dc52c0674e732
monkey_kid03 <135007348+huggywuggybest03@users.noreply.github.com>
2023-06-25 13:15:41 +0100

    Update Executor.lua

--- Executor.lua
+++ Executor.lua
@@ -1,2 +1,2 @@
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("THIS WAS CODED BY monkey_kid03 ON ROBLOX | JJSploit on top | Fluxus on top | Delta on top","All")
---Outdated, check JJSploit.lua
+--Outdated.
