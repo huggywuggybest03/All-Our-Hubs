c8151843f26d3da89c48aec7691ce8dd89c27605
monkey_kid03 <135007348+huggywuggybest03@users.noreply.github.com>
2023-06-25 19:03:39 +0100

    Update and rename UI Libraries.txt to UI Libraries.lua

---
+++ UI-Libraries/UI Libraries.lua
@@ -0,0 +1,40 @@
+--Libraries I knew
+
+--1. LinoriaLib
+--https://github.com/violin-suzutsuki/LinoriaLib/blob/main/Example.lua
+
+--2. Ocerium-Project
+--https://github.com/slf0Dev/Ocerium_Project/blob/main/README.md
+
+--3. Rayfield
+--https://docs.sirius.menu/rayfield --ofc this looks different, dont worry.
+
+--4. Kavo UI
+--https://xheptcofficial.gitbook.io/kavo-library/ --same here, but dont worry!
+
+--5. Orion
+--https://github.com/shlexware/Orion/blob/main/Documentation.md
+
+--6. Wally
+--https://github.com/bloodball/UI-Librarys/blob/main/Wally%20UI%20v3
+
+--New libraries | 06/21/23
+
+--1. Venyx
+--https://github.com/GreenDeno/Venyx-UI-Library/blob/main/example.lua
+
+--2. Kronos
+--https://raw.githubusercontent.com/bloodball/UI-Librarys/main/Kronos | yes, it is raw.
+
+--3. Glitch Drawing Library
+--https://github.com/JohnBardot/GlitchDrawingLibrary/blob/main/Documentation.md
+
+--New libraries | 06/22/23
+
+--1. Atlas
+--https://docs.siegehub.net/Atlas | ofc this looks familiar to rayfield link.
+
+--New libraries | 06/25/23
+
+--1. Fluxus UI
+--https://raw.githubusercontent.com/huggywuggybest03/All-Our-Hubs/main/UI-Libraries/Fluxus%20UI.lua
